f1c295e1fdfb10402e7c7fd0df4c92c93a9048c3
Noel Alejandro <noel_ska15@hotmail.com>
2015-02-13 00:21:22 -0600

    Cambiado contexto para imprimir mensajes en consola de webkit

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,13 @@ var Product = require('../models/products');
 
 /* GET home page. */
 router.get('/', function(req, res) {
+  window.console.log("Mi log");
   res.render('index', { title: 'Express' });
 });
 
 
 router.get('/nueva', function(req, res) {
+  window.console.log("Nueva log");
   res.render('index2', { title: 'Express' });
 });
 
@@ -16,7 +18,7 @@ router.post('/new', function(req, res) {
   // Creamos una instancia del objeto Product con información desde el inicio
   // We create a Product object instance with information since beginning 
   var product =  new Product(req.param('product'));
-  console.log(product.category);
+  window.console.log(product.category);
   // Con esta funcion guardamos el producto en el modelo Product
   // This function save the prouducto object in Product model
   product.save(function (err, product) {
@@ -24,7 +26,7 @@ router.post('/new', function(req, res) {
       return console.error(err);
     }
     else{
-      console.error("Guardado con exito");
+      window.console.error("Guardado con exito");
       res.redirect('/products');
     }
   });
